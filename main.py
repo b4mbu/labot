789bdb6e116ccf7278306fe631454da64b26f147
Glushin Nikita <nikita.glushin@yandex.ru>
2023-10-25 21:05:02 +0300

    added config

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from src.telegrambot import telegram
+import asyncio
+from src.telegram import telegram
+from src.database import db_session
+from config import config
+
+if __name__ == "__main__":
+    config = config.Config() 
+
+    db_session.global_init(config.database)
+    asyncio.run(telegram.start_polling(config.telegram))
--- src/database/db_session.py
+++ src/database/db_session.py
@@ -1,21 +1,23 @@
+from pydantic import config
 import sqlalchemy as sa
 import sqlalchemy.ext.declarative as dec
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
+from config import config
 
 SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
 
 
-def global_init():
+def global_init(config : config.DatabaseConfig):
     global __factory
 
     if __factory:
         return
 
-    conn_str = f'postgresql://postgres:O(1)@194.58.111.234:5432/labot'
-    print(f'Подключение к базе данных по адресу {conn_str}')
+    conn_str = f'postgresql://{config.username}:{config.password}@{config.host}:{config.port}/{config.db_name}'
+    print(f'Connected to database {conn_str}')
 
     engine = sa.create_engine(conn_str)
     __factory = orm.sessionmaker(bind=engine)
