3ed9929f5088c1cc2205ee2997ad705e4b210e03
godofcs <chesnov.ilya@bk.ru>
2023-10-25 19:20:57 +0300

    My first commit

--- src/database/db_session.py
+++ src/database/db_session.py
@@ -1,29 +1,23 @@
-from pydantic import config
 import sqlalchemy as sa
 import sqlalchemy.ext.declarative as dec
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
-from config import config
-from src.database.database_scheme import create_metadata
 
 SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
 
 
-def global_init(config: config.DatabaseConfig):
+def global_init():
     global __factory
 
     if __factory:
         return
 
-    conn_str = f'postgresql://{config.username}:{config.password}@{config.host}:{config.port}/{config.db_name}'
-    print(f'Connected to database {conn_str}')
+    conn_str = f'postgresql://postgres:NP9IGumJsdIvnDfVkOd0@containers-us-west-192.railway.app:6504/railway'
+    print(f'Подключение к базе данных по адресу {conn_str}')
 
     engine = sa.create_engine(conn_str)
-    #Следующие две строчки нужно раскомментировать, только если вы хотите создать пустую базу данных
-    #metadata = create_metadata()
-    #metadata.create_all(engine)
     __factory = orm.sessionmaker(bind=engine)
 
     SqlAlchemyBase.metadata.create_all(engine)
