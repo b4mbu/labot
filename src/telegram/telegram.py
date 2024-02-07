0f84a2e81503d8cc5e0a209c12e5bb6a7aa912d0
niqote <niqote@yandex-team.ru>
2024-02-07 20:50:50 +0400

    states

--- src/telegram/telegram.py
+++ src/telegram/telegram.py
@@ -19,20 +19,20 @@ import asyncio
 
 
 class Gen(StatesGroup):
-    just_chill               = State()
-    # регистрация 
-    auth_wait_token          = State()
-    auth_wait_full_name      = State()
+    just_chill = State()
 
-    # создание лабы
-    wait_lab_data            = State()
+    # authentification 
+    auth_wait_token     = State()
+    auth_wait_full_name = State()
 
-    # удаление токена
-    remove_wait_token        = State() 
+    # create lab 
+    create_lab_wait_data = State()
 
-    # просмотр доступных вариантов
-    show_var_wait_lab_name = State()
+    # remove token 
+    token_remove_wait_token = State() 
 
+    # choose variant
+    variant_wait_name = State() 
 
 
 dp = Dispatcher()
@@ -79,7 +79,7 @@ async def cmd_authorization(message: types.Message, state: FSMContext):
 @dp.message(Command("add_lab"))
 async def cmd_add_lab(message: types.Message, state: FSMContext):
     await message.answer("Введите данные лабораторной работы в формате:\n<название>\n\n<описание>")
-    await state.set_state(Gen.wait_lab_data)
+    await state.set_state(Gen.create_lab_wait_data)
 
 @dp.message(Command("show_labs"))
 async def cmd_show_labs(message: types.Message, state: FSMContext):
@@ -120,7 +120,7 @@ async def cmd_remove_token(message: types.Message, state: FSMContext):
         await message.answer("Пока нет доступа(")
         return
     await message.answer("Введите токен, который нужно удалить: <token>")
-    await state.set_state(Gen.remove_wait_token)
+    await state.set_state(Gen.token_remove_wait_token)
 
 @dp.message(Command("show_vars"))
 async def cmd_show_vars(message: types.Message, state: FSMContext):
@@ -147,8 +147,8 @@ async def auth_new_user(message: types.Message, state: FSMContext):
     await message.answer("Вход выполнен")
     await state.clear()    # удалит данные в state (user_data["role"]) -> removed
 
-@dp.message(F.text, Gen.wait_lab_data)
-async def create_new_lab(message: types.Message, state: FSMContext):
+@dp.message(F.text, Gen.create_lab_wait_data)
+async def create_new_lab(message: types.Message):
     text = message.text
     splitted = text.split('\n')
     name = splitted[0]
@@ -159,8 +159,8 @@ async def create_new_lab(message: types.Message, state: FSMContext):
         return
     await message.answer("Произошла ошибка")
 
-@dp.message(F.text, Gen.remove_wait_token)
-async def remove_token_from_table(message: types.Message, state: FSMContext):
+@dp.message(F.text, Gen.token_remove_wait_token)
+async def remove_token_from_table(message: types.Message):
     if not is_admin(message.from_user.id):
         await message.answer("Пока нет доступа(")
         return
